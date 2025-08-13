16b52e983fd455be5a1e8da185799c9654c883e0
Tia Piol <tiarox02@yahoo.com>
2025-08-13 12:22:21 -0700

    post lab completed

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -7,8 +7,9 @@ const router = express.Router();
 router.post("/", async (req, res) => {
     try {
         const createdTrack = await Track.create(req.body)
+        res.status(201).json(createdTrack);
     } catch (err) {
-
+        res.status(500).json({ err: err.message });
     }
 });
 
