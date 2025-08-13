63a37b2473e2b1e130e932b64f9312557a6914d7
Tia Piol <tiarox02@yahoo.com>
2025-08-13 12:26:54 -0700

    completed index route

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -15,6 +15,17 @@ router.post("/", async (req, res) => {
 
 // GET /tracks          | INDEX
 
+router.get("/", async (req, res) => {
+    try {
+        const foundTracks = await Track.find();
+        res.status(200).json(foundTracks);
+
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+
+});
+
 // GET /tracks/:id      | SHOW
 
 // PUT /tracks/:id      | UPDATE
